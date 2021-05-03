129dc8e1be74b396cf875523b345425c16143163
javi <javitgomez@gmail.com>
2021-05-03 11:19:58 +0200

    restajav-009-config

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use App\Repository\EventRepository;
+use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,10 +18,13 @@ class HomeController extends AbstractController
      * @Route("/", name="user_index", methods={"GET"})
      * @return Response
      */
-    public function index(EventRepository $eventRepository): Response
+    public function index(EventRepository $eventRepository, ImageRepository $imageRepository): Response
     {
+        $images = $imageRepository->findAll();
+
         return $this->render('front/index.html.twig', [
-            'events' => $eventRepository->findAll()
+            'events' => $eventRepository->findAll(),
+             'images' => $images
         ]);
     }
 }
