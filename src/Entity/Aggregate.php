ad256b6d82a3a372e4471418f89ad5c98389b871
inforob <inforob@ono.com>
2021-05-06 12:52:07 +0200

    RESTAJAV-002 - Añadir Entities

---
+++ src/Entity/Aggregate.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AggregateRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AggregateRepository::class)
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Aggregate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=70)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $required;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Dish::class, inversedBy="aggregate")
+     */
+    private $dish;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getRequired(): ?bool
+    {
+        return $this->required;
+    }
+
+    public function setRequired(bool $required): self
+    {
+        $this->required = $required;
+
+        return $this;
+    }
+
+    public function getDish(): ?Dish
+    {
+        return $this->dish;
+    }
+
+    public function setDish(?Dish $dish): self
+    {
+        $this->dish = $dish;
+
+        return $this;
+    }
+}
