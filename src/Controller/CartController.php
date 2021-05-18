eb317290a33c6d41435ec16c4b834459e02c4aaf
inforob <inforob@ono.com>
2021-05-18 12:27:46 +0200

    RESTAJAV-006 - Carrito Opciones

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,8 +6,10 @@ use App\Entity\CartDish;
 use App\Repository\CartDishRepository;
 use App\Repository\DishRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -73,17 +75,56 @@ class CartController extends AbstractController
         return new JsonResponse(['error' => false, 'msg'=> 'success', 'code' => 200 , 'data'  => ['quantity' => $quantity] ]);
     }
 
+    /**
+     * @Route("/del/dish/{dishId}" , name="del_dish_to_cart" )
+     * @throws \Exception
+     */
+    public function deleteDishToCart(CartDishRepository $cartDishRepository, $dishId): RedirectResponse
+    {
+        $tokenIdSession = $this->session->get('_cart');
+
+        $dishOrder = $cartDishRepository
+            ->findOneBy(['sessionId' => $tokenIdSession, 'dishId' => $dishId]);
+        if (!$dishOrder) {
+            throw new Exception('simon says: you cant not enter here');
+        }
+        $dishOrder->setQuanty($dishOrder->getQuanty() - 1);
+        $this->em->remove($dishOrder);
+        $this->em->flush();
+
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/update/dish/{dishId}/{quanty}" , name="update_dish_to_cart" , options={"expose"=true} )
+     * @throws \Exception
+     */
+    public function updateDishToCart(CartDishRepository $cartDishRepository, $dishId, $quanty): RedirectResponse
+    {
+        $tokenIdSession = $this->session->get('_cart');
+
+        $dishOrder = $cartDishRepository
+            ->findOneBy(['sessionId' => $tokenIdSession, 'dishId' => $dishId]);
+        if (!$dishOrder) {
+            throw new Exception('simon says: you cant not enter here');
+        }
+        $dishOrder->setQuanty($quanty);
+        $this->em->persist($dishOrder);
+        $this->em->flush();
+
+        return $this->redirectToRoute('cart');
+    }
+
 
     /**
     * @Route("/" , name="cart" , options={"expose"=true} )
     * @throws \Exception
     */
-    public function cart(Request $request, CartDishRepository $cartDishRepository)
+    public function cart(CartDishRepository $cartDishRepository): Response
     {
         $tokenIdSession = $this->session->get('_cart');
         $cart = $cartDishRepository
             ->findBy(['sessionId' => $tokenIdSession]);
-
 
         return $this->render(
             'cart/index.html.twig',
